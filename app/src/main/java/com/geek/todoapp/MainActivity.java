c487e08b0f16eb7be780b0986fa3e630dfad5dc1
yryskeldi03 <ymayrambekov@mail.ru>
2021-06-26 14:28:45 +0600

    new

--- app/src/main/java/com/geek/todoapp/MainActivity.java
+++ app/src/main/java/com/geek/todoapp/MainActivity.java
@@ -1,21 +1,30 @@
 package com.geek.todoapp;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.geek.todoapp.databinding.ActivityMainBinding;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private NavController navController;
+    private ArrayList<Integer> fragments = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,28 @@ public class MainActivity extends AppCompatActivity {
         navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        navController.navigate(R.id.boardFragment);
+
+        fragments.add(R.id.navigation_home);
+        fragments.add(R.id.navigation_dashboard);
+        fragments.add(R.id.navigation_notifications);
+        fragments.add(R.id.profileFragment2);
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull @NotNull NavController controller, @NonNull @NotNull NavDestination destination, @Nullable @org.jetbrains.annotations.Nullable Bundle arguments) {
+                if (fragments.contains(destination.getId())) {
+                    navView.setVisibility(View.VISIBLE);
+                } else
+                    navView.setVisibility(View.GONE);
+
+                if (destination.getId() == R.id.boardFragment) {
+                    getSupportActionBar().hide();
+                } else
+                    getSupportActionBar().show();
+            }
+        });
     }
 
     @Override
